17961672f68086d994d85814ec7dfc8851570d7a
JongHyunJung <jjh19960115@gmail.com>
2024-02-22 20:08:33 +0900

    [improve] 백준 silver 3 1

---
+++ BackJoon/4375/solution_1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main() {
+    while (cin >> n) {
+        int cnt = 1, ret = 1;
+
+        while(true){
+            if(cnt % n == 0) {
+                printf("%d\n", ret);
+                break;
+            } else {
+                cnt = (cnt * 10) + 1;
+                cnt %= n;
+                ret++;
+            }
+        }
+    }
+
+    return 0;
+}
