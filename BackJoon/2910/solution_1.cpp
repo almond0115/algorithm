b1dd69f75eefb1657288edcdd5956a43f6e1f171
JongHyunJung <jjh19960115@gmail.com>
2024-02-29 23:03:46 +0900

    [improve] 백준 silver 3 빈도 정렬

---
+++ BackJoon/2910/solution_1.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define pp pair<int, int>
+typedef long long ll;
+using namespace std;
+
+int n, c, a[1004];
+vector<pp> v;
+map<int, int> mp, mp_first;
+
+bool cmp(pp a, pp b){
+    if(a.first == b.first){
+        return mp_first[a.second] < mp_first[b.second];
+    }
+    
+    return a.first > b.first;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> c;
+    for(int i=0 ; i<n ; i++){
+        cin >> a[i];
+        mp[a[i]]++;
+        if(mp_first[a[i]] == 0) mp_first[a[i]] = i+1;
+    }
+
+    for(auto it : mp){
+        v.push_back({it.second, it.first});
+    }
+
+    sort(v.begin(), v.end(), cmp);
+    for(auto i : v){
+        for(int j=0 ; j<i.first ; j++){
+            cout << i.second << " ";
+        }
+    }
+
+    return 0;
+}
