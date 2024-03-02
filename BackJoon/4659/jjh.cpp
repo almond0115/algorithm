ca9aefc7e52f5e97ab133650c80ac1dbe416f4e4
JongHyunJung <jjh19960115@gmail.com>
2024-03-02 21:18:45 +0900

    [improve] 백준 silver 5 비밀번호 발음하기

--- BackJoon/4659/jjh.cpp
+++ BackJoon/4659/jjh.cpp
@@ -5,39 +5,44 @@ using namespace std;
 string n;
 
 bool pwd_reader(string pwd){
-    cout << "enter pwd_reader" << '\n';
-    int vowel = 0, consonant = 0;
+    // cout << "enter pwd_reader" << '\n';
+    int vowel = 0, consonant = 0, cnt = 0;
     for(int i=0 ; i<pwd.length() ; i++){
-        if(pwd[i] < 'a' || pwd[i] > 'z') break;
-        cout << "pass a to z" << '\n';
+        if(pwd[i] >= 'A' && pwd[i] <= 'Z') break;
+        // cout << "pass a to z" << '\n';
         if(pwd[i] == 'a' || pwd[i] == 'e' || pwd[i] == 'i' || pwd[i] == 'o' || pwd[i] == 'u'){
+            cnt++;
             vowel++;
             consonant = 0;
         } else {
             consonant++;
             vowel = 0;
         }
-        cout << "pass vowel, consonant test" << '\n';
-        // if(i>=1 && (pwd[i] == pwd[i-1]) && (pwd[i] == 'e' || pwd[i] == 'o')) {
-        //     cout << pwd[i-1] << pwd[i] << '\n';
-        //     continue;
-        // }
-        // cout << "pass e, o test" << '\n';
-        cout << "현재 i 값 : " << i << ", " << "pwd[i-1] : " << pwd[i-1] << ", " << "pwd[i] : " << pwd[i] << '\n';
+        // cout << "pass vowel, consonant test : " << vowel << '\n';
+     
+        // cout << "현재 i 값 : " << i << ", " << "pwd[i-1] : " << pwd[i-1] << ", " << "pwd[i] : " << pwd[i] << '\n';
         if(i>=1 && (pwd[i] == pwd[i-1])) {
-            cout << "pwd[i] : " << pwd[i] << '\n';
+            // cout << "pwd[i] : " << pwd[i] << '\n';
             if(pwd[i] == 'e' || pwd[i] == 'o') {
-                cout << "[e o]" << '\n';
+                // cout << "[e o]" << '\n';
+                if(i == pwd.length() - 1) {
+                    if(cnt == 0) break;
+                    return true;
+                }
                 continue;
             }
             break;
         } 
-        cout << "pass e, o test" << '\n';
+        // cout << "pass e, o test" << '\n';
         if(vowel == 3 || consonant == 3) break;
-        cout << "pass dup test" << '\n';
-        if(i == pwd.length() - 1) return 0;
+        // cout << "pass dup test" << '\n';
+        // if(i == pwd.length() -1 && cnt == 0) break;
+        if(i == pwd.length() - 1) {
+            if(cnt == 0) break;
+            return true;
+        }
     }
-    return 1;
+    return false;
 };
 
 int main() {
@@ -45,7 +50,7 @@ int main() {
 
     while(cin >> n){
         if(n == "end") break;
-        if(n.length() >= 1 && n.length() <= 20) pwd_reader(n);
+        if(n.length() < 1 || n.length() > 20) break;
         if(pwd_reader(n)) {
             cout << "<" << n << "> " << "is acceptable." << '\n';
         } else  {
