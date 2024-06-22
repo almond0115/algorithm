8ab8cba42dfda8d5999a09e9e77f599db027b5b2
JongHyunJung <jjh19960115@gmail.com>
2024-06-22 23:59:41 +0900

    Update README.md

--- leetcode/medium/869/jjh.py
+++ leetcode/medium/869/jjh.py
@@ -7,7 +7,7 @@ def is_power_of_two(n):
         n /= 2
     return n == 1
     
-class Solution:
+class Solution_1:
     def reorderedPowerOf2(self, n: int) -> bool:
         # 주어진 문자열의 문자들을 조정하여 2의 거듭제곱 있으면 true, 아니면 false
         # str_n = []
@@ -21,3 +21,14 @@ class Solution:
                 if is_power_of_two(num):
                     return True
         return False
+
+class Solution_2:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        # 제한 조건에서 최대 10억이므로 2의 30승까지 배열에 정리
+        power_of_twos = [str(1 << i) for i in range(31)]
+        # 2의 30승까지 값들을 정렬하여 n 탐색 전 세팅
+        sorted_pOf2 = [''.join(sorted(p)) for p in power_of_twos]
+
+        # n 값을 정렬
+        sorted_n = ''.join(sorted(str(n)))
+        return sorted_n in sorted_pOf2 
